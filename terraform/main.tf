1ef4eb141f667620b03bfd24844a5f6f3804e211
Ahmed Rafat <ahmedrafaat456@example.com>
2025-08-01 20:04:52 +0300

    push lamba edit

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,7 +35,7 @@ module "notify_lambda" {
 }
 
 resource "aws_instance" "test" {
-  ami           = "ami-0c02fb55956c7d316"  # مثال على AMI أوبونتو في us-east-1، ممكن تغيره حسب الريجن
+  ami           = "ami-0c02fb55956c7d316"  
   instance_type = "t2.micro"
   tags = {
     Name = "test"
