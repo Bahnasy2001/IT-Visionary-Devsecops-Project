a4b24b482a371fc28d88cf4bbe7107849c7786c3
Ahmed Rafat <ahmedrafaat456@example.com>
2025-08-03 00:49:31 +0300

    edit the policy

--- terraform/modules/elb/main.tf
+++ terraform/modules/elb/main.tf
@@ -60,7 +60,7 @@ resource "aws_s3_bucket_public_access_block" "alb_logs" {
   restrict_public_buckets = true
 }
 
-# S3 bucket policy for ALB access
+## S3 bucket policy for ALB access
 resource "aws_s3_bucket_policy" "alb_logs" {
   bucket = aws_s3_bucket.alb_logs.id
 
