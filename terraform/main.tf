088aebaeb89c5f26f3cc01538766e694dcfd9e4a
Ahmed Rafat <ahmedrafaat456@example.com>
2025-08-02 12:00:27 +0300

    remove ec2 test

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,10 +34,3 @@ module "notify_lambda" {
   
 }
 
-resource "aws_instance" "test" {
-  ami           = "ami-0c02fb55956c7d316"  
-  instance_type = "t2.micro"
-  tags = {
-    Name = "test"
-  }
-}
