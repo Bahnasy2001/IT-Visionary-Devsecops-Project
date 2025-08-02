fde809223f70ee37413472c5ad7368896b2fca80
AmroFouda <amr.fouda.284@gmail.com>
2025-08-02 13:34:10 +0300

    Update variables.tf adding network

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -103,3 +103,46 @@ variable "lb_logging_bucket" {
   description = "S3 bucket for ALB access logs"
   type        = string
 }
+
+#network 
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "my-project"
+}
+
+variable "environment" {
+  description = "Environment name (dev, staging, prod, etc.)"
+  type        = string
+  default     = "dev"
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+} 
