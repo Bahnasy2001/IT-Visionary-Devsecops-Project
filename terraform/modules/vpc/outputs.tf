f1cf3d509856993d7cc23352fad13e2d20e1b252
AmroFouda <amr.fouda.284@gmail.com>
2025-08-02 14:03:11 +0300

    Update outputs.tf

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -37,7 +37,6 @@ output "security_group_ids" {
   description = "Map of security group IDs"
   value = {
     web = aws_security_group.web.id
-    app = aws_security_group.app.id
-    db  = aws_security_group.db.id
   }
+
 } 
