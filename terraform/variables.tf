7df40f599d5b55ed8924430930f20fd6b2288884
Ahmed Rafat <ahmedrafaat456@example.com>
2025-08-03 13:38:54 +0300

    push the key

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -68,11 +68,6 @@ variable "target_type" {
 }
 
 #network 
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
-}
 
 variable "project_name" {
   description = "Name of the project"
